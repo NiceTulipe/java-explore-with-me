1b248eaf19bd6c2ae95ac44ebb44b31f00417a32
NiceTulipe <lyubovchenko@yandex.ru>
2023-07-05 16:42:57 +0300

    tz diplom fixed

--- ewm-statistic/statistic-service/src/main/java/ru/practicum/stats/stats/StatsServer.java
+++ ewm-statistic/statistic-service/src/main/java/ru/practicum/stats/stats/StatsServer.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsServer {
+public class  StatsServer {
     @Generated
     public static void main(String[] args) {
         SpringApplication.run(StatsServer.class, args);
