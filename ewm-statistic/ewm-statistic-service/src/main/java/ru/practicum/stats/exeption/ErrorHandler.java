471b9c67019badb3c934038dead3d8c02a607819
NiceTulipe <lyubovchenko@yandex.ru>
2023-07-16 01:39:06 +0300

    main week2_almost_v4_crashing_idk_reason i`m tired hope i will did it_4

--- ewm-statistic/ewm-statistic-service/src/main/java/ru/practicum/stats/exeption/ErrorHandler.java
+++ ewm-statistic/ewm-statistic-service/src/main/java/ru/practicum/stats/exeption/ErrorHandler.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ValidationException;
-import java.util.Map;
-
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
